023efdff7c0e31e51acec78ad040ef4978c4eea0
lewis_mills <lewismills290@protonmail.com>
2025-06-17 14:44:10 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,77 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
+
+const game = new Game();
 
 // Write your code here
+const button = document.querySelector('.button');
+const cells = document.querySelectorAll('.field-cell');
+const startMessage = document.querySelector('.message-start');
+const score = document.querySelector('.game-score');
+const win = document.querySelector('.message-win');
+const lose = document.querySelector('.message-lose');
+
+function updateUI() {
+  for (let i = 0; i < cells.length; i++) {
+    const row = Math.floor(i / 4);
+    const col = i % 4;
+    const value = game.board[row][col];
+
+    cells[i].textContent = value === 0 ? '' : value;
+    cells[i].className = `field-cell field-cell--${value}`;
+  }
+}
+
+button.addEventListener('click', () => {
+  startMessage.classList.add('hidden');
+  lose.classList.add('hidden');
+
+  if (button.classList.contains('start') === true) {
+    game.start();
+    updateUI();
+
+    button.classList.remove('start');
+    button.classList.add('restart');
+    button.textContent = 'Reset';
+  } else {
+    game.restart();
+    updateUI();
+
+    button.classList.remove('restart');
+    button.classList.add('start');
+    button.textContent = 'Start';
+    startMessage.classList.remove('hidden');
+  }
+
+  score.textContent = game.getScore() || 0;
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowLeft') {
+    game.moveLeft();
+    updateUI();
+    score.textContent = game.getScore();
+  } else if (e.key === 'ArrowUp') {
+    game.moveUp();
+    updateUI();
+    score.textContent = game.getScore();
+  } else if (e.key === 'ArrowRight') {
+    game.moveRight();
+    updateUI();
+    score.textContent = game.getScore();
+  } else if (e.key === 'ArrowDown') {
+    game.moveDown();
+    updateUI();
+    score.textContent = game.getScore();
+  }
+
+  if (game.hasWon()) {
+    win.classList.remove('hidden');
+  }
+
+  if (!game.canMove()) {
+    lose.classList.remove('hidden');
+  }
+});
